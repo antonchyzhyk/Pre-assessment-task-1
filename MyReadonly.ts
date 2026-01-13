7beaefabe98bc297e62c312aeb2d7f669179498c
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-13 19:44:43 +0200

    complete readonly task

---
+++ MyReadonly.ts
@@ -0,0 +1,16 @@
+interface Todo {
+  title: string
+  description: string
+}
+
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K]
+}
+
+const todo2: MyReadonly<Todo> = {
+  title: "Hey",
+  description: "foobar"
+}
+
+todo2.title = "Hello" // Error: cannot reassign a readonly property
+todo2.description = "barFoo" // Error: cannot reassign a readonly property
